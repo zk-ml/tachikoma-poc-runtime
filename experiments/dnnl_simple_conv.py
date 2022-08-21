e873902883e27f5a1a867a815e827ccb538a465d
Peiyuan Liao <peiyuan@praxispioneering.com>
2022-08-21 15:08:55 -0400

    Update dnnl_simple_conv.py

--- experiments/dnnl_simple_conv.py
+++ experiments/dnnl_simple_conv.py
@@ -12,7 +12,7 @@ scale = 100
 data = numpy.random.uniform(-scale, scale, size=dshape).astype(eltype)
 kern = numpy.random.uniform(-scale, scale, size=kshape).astype(eltype)
 
-w = relay.var("weight", shape=kshape, dtype="float32")
+w = relay.var("weight", shape=kshape, dtype=eltype)
 x = relay.var("x", shape=dshape, dtype=eltype)
 y = relay.nn.conv2d(
     x, w, padding=(1, 1), dilation=(1, 1), groups=1, channels=1, kernel_size=(1, 1)
@@ -22,7 +22,10 @@ z = relay.nn.relu(y)
 mod = tvm.IRModule()
 mod["main"] = relay.Function([x, w], z)
 
-mod = relay.transform.MergeComposite(pattern_table())(mod)
+patterns = pattern_table()
+print(patterns)
+
+mod = relay.transform.MergeComposite(patterns)(mod)
 mod = relay.transform.AnnotateTarget(["dnnl"])(mod)  # Output: Figure 2
 mod = relay.transform.MergeCompilerRegions()(mod)  # Output: Figure 3
 mod = relay.transform.PartitionGraph()(mod)  # Output: Figure 4
