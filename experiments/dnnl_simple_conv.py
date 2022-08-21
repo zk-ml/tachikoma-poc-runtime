9e38c922b5dcfc65355a45312fe891d7e9e9ee66
Peiyuan Liao <peiyuan@praxispioneering.com>
2022-08-21 15:52:50 -0400

    cpu runtime

--- experiments/dnnl_simple_conv.py
+++ experiments/dnnl_simple_conv.py
@@ -4,6 +4,19 @@ from tvm.relay.op.contrib.dnnl import pattern_table
 from tvm import relay
 from tvm.relay import testing
 import tvm.contrib.graph_executor as runtime
+import os
+import sys
+import numpy as np
+import tvm
+from tvm.relay.backend import te_compiler
+from tvm.relay.backend.runtime import Runtime
+import tvm.relay.testing
+from tvm import relay
+from tvm import runtime as tvm_runtime
+from tvm.relay import transform
+from tvm.contrib import utils
+import onnx
+from tvm.contrib.download import download_testdata
 
 dtype = "float32"
 scale = 1
@@ -49,11 +62,39 @@ print(mod["main"].astext(show_meta_data=False), "\n")
 mod = relay.transform.PartitionGraph()(mod)
 print(mod["main"].astext(show_meta_data=False), "\n")
 
+map_inputs = {"a": da, "b": db}
 with tvm.transform.PassContext(opt_level=0):
-    graph, module, params = relay.build(mod, target="llvm", params={"a": da, "b": db})
+    graph, lib, params = relay.build(mod, target="llvm", params=map_inputs)
 
 with open("graph.json", "w") as f:
     f.write(graph)
 
+print(graph, lib, params)
 
-print(graph, module, params)
+
+def update_lib(lib):
+    source_dir = "/data/catalyst/tachikoma-tvm"
+    contrib_path = os.path.join(source_dir, "src", "runtime", "contrib")
+    kwargs = {}
+    kwargs["options"] = ["-O2", "-std=c++14", "-I" + contrib_path]
+    tmp_path = utils.tempdir()
+    lib_name = "lib.so"
+    lib_path = tmp_path.relpath(lib_name)
+    lib.export_library(lib_path, fcompile=False, **kwargs)
+    lib = tvm_runtime.load_module(lib_path)
+
+    return lib
+
+
+device = tvm.cpu()
+lib = update_lib(lib)
+rt_mod = tvm.contrib.graph_executor.create(graph, lib, device)
+for name, data in map_inputs.items():
+    rt_mod.set_input(name, data)
+rt_mod.run()
+
+out_shapes = [[1, 10]]
+
+for idx, shape in enumerate(out_shapes):
+    out = tvm.nd.empty(shape, device=device)
+    out = rt_mod.get_output(idx, out)
