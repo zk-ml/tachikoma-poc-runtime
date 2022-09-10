7b4025d7c656bc295d0709a195b256f18fd0b791
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-09-09 20:09:29 -0400

    Update ibert_transformers_pytorch.py

--- experiments/ibert_transformers_pytorch.py
+++ experiments/ibert_transformers_pytorch.py
@@ -1,9 +1,15 @@
 import torch
+import torch.onnx
 import tvm
 from transformers import AutoTokenizer, AutoModelForMaskedLM
 from tvm import relay
 import numpy as np
+import onnx
 
+model_path = "ibert.onnx"
+pytorch = False
+
+input_name = "input_ids"
 model_name = "kssteven/ibert-roberta-base"  # "bert-base-uncased"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForMaskedLM.from_pretrained(model_name, return_dict=False)
@@ -30,31 +36,44 @@ shape_list = [
 print(traced_model)
 print(shape_list)
 
-if False:
+torch.onnx.export(
+    traced_model,  # model being run
+    inputs,  # model input (or a tuple for multiple inputs)
+    model_path,  # where to save the model (can be a file or file-like object)
+    export_params=True,
+    opset_version=10,  # the ONNX version to export the model to
+    do_constant_folding=False,  # whether to execute constant folding for optimization
+    input_names=["input_name"],  # the model's input names
+    output_names=["output"],  # the model's output names
+)
+
+if pytorch:
     mod, params = relay.frontend.pytorch.from_pytorch(
         traced_model, shape_list, default_dtype="int8"
     )
 
-    # with relay.quantize.qconfig(calibrate_mode="global_scale", global_scale=127.0):
-    #    mod = relay.quantize.quantize(mod, params)
+else:
+    onnx_model = onnx.load(model_path)
+    mod, params = relay.frontend.from_onnx(onnx_model, shape_list)
 
-    print(mod)
+# with relay.quantize.qconfig(calibrate_mode="global_scale", global_scale=127.0):
+#    mod = relay.quantize.quantize(mod, params)
 
-    target = tvm.target.Target("llvm", host="llvm")
-    dev = tvm.cpu(0)
-    with tvm.transform.PassContext(opt_level=0):
-        lib = relay.build(mod, target=target, params=params)
+print(mod)
 
-    from tvm.contrib import graph_executor
+target = tvm.target.Target("llvm", host="llvm")
+dev = tvm.cpu(0)
+with tvm.transform.PassContext(opt_level=0):
+    lib = relay.build(mod, target=target, params=params)
 
-    input_name = "input_ids"
+from tvm.contrib import graph_executor
 
-    m = graph_executor.GraphModule(lib["default"](dev))
-    # Set inputs
-    m.set_input(input_name, tvm.nd.array(inputs.numpy()))
-    # Execute
-    m.run()
-    # Get outputs
-    tvm_output = m.get_output(0).numpy()
+m = graph_executor.GraphModule(lib["default"](dev))
+# Set inputs
+m.set_input(input_name, tvm.nd.array(inputs.numpy()))
+# Execute
+m.run()
+# Get outputs
+tvm_output = m.get_output(0).numpy()
 
-    print(np.absolute(tvm_output - torch_output))
+print(np.absolute(tvm_output - torch_output))
