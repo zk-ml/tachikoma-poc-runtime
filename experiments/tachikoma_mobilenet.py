24b18a5d31c6db9ea8db968a6c9b736298d7fff2
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-09-16 22:50:43 -0400

    Update tachikoma_mobilenet.py

--- experiments/tachikoma_mobilenet.py
+++ experiments/tachikoma_mobilenet.py
@@ -31,6 +31,8 @@ rt_mod = tvm.contrib.graph_executor.GraphModule(lib["default"](device))
 
 print("subsequent runs")
 for i in range(2):
+    path_set(explib, f"/data/tachikoma_results/serialized_{i}.ndarray")
+    
     for name, data in lib.get_params().items():
         print(name, data.shape)
         data = tvm.nd.array(data.numpy() + i)
@@ -38,5 +40,3 @@ for i in range(2):
     rt_mod.run()
 
     out = rt_mod.get_output(0)
-
-    path_set(explib, f"/data/tachikoma_results/serialized_{i}.ndarray")
