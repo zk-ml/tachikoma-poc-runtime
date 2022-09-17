4d3c4243e162e9ef59ba704cd960a680eeb257f2
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-09-17 16:52:15 -0400

    Update tachikoma_mobilenet.py

--- experiments/tachikoma_mobilenet.py
+++ experiments/tachikoma_mobilenet.py
@@ -10,10 +10,7 @@ target = "llvm"
 dtype = "float32"
 ishape = (1, 3, 224, 224)
 mod, params = relay.testing.mobilenet.get_workload(batch_size=1, dtype="float32")
-#print(mod["main"].astext(show_meta_data=False), "\n")
-#mod = transform.AnnotateTarget(["tachikoma"])(mod)
-#mod = transform.MergeCompilerRegions()(mod)
-#mod = transform.PartitionGraph()(mod)
+print(mod["main"].astext(show_meta_data=False), "\n")
 mod = tachikoma.partition_for_tachikoma(mod, params)
 print(mod["main"].astext(show_meta_data=False), "\n")
 print(mod.get_global_vars())
@@ -30,15 +27,12 @@ rmod = lib["default"](device)
 #print(type(lib["default"]))
 print(lib)
 print(explib)
-print(rmod)
-print(type(rmod))
+print(type(explib))
 
 rt_mod = tvm.contrib.graph_executor.GraphModule(rmod)
 
 print("subsequent runs")
 for i in range(2):
-    #explib["TachikomaSetExportPath"](f"/data/tachikoma_results/serialized_{i}.ndarray")
-    path_set(rmod, f"/data/tachikoma_results/serialized_{i}.ndarray")
     path_set(explib, f"/data/tachikoma_results/serialized_{i}.ndarray")
     
     for name, data in lib.get_params().items():
