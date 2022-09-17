599de03461e10c188a23f30e2f56f0754ceeb35d
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-09-17 19:28:18 -0400

    Update tachikoma_bert.py

--- experiments/tachikoma_bert.py
+++ experiments/tachikoma_bert.py
@@ -4,12 +4,49 @@ from tvm.relay.op.contrib import tachikoma
 from tvm import relay
 from tvm.relay import build_module
 import tvm.relay.testing
+import torch
+import torch.onnx
+import tvm
+from transformers import AutoTokenizer, AutoModelForMaskedLM
+from tvm import relay
+import numpy as np
+
+input_name = "input_ids"
+model_name = "bert-base-uncased" # "kssteven/ibert-roberta-base"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForMaskedLM.from_pretrained(
+    model_name, return_dict=False, torchscript=True # quant_mode=True, 
+)
+
+text = "I'm sorry, Dave. [MASK]"
+inputs = tokenizer(text, return_tensors="pt")["input_ids"]
+
+model.eval()
+for p in model.parameters():
+    p.requires_grad_(False)
+
+torch_output = model(inputs)[0].numpy()
+
+traced_model = torch.jit.script(model, inputs)
+traced_model.eval()
+
+for p in traced_model.parameters():
+    p.requires_grad_(False)
+
+shape_list = [
+    (i.debugName().split(".")[0], i.type().sizes())
+    for i in list(traced_model.graph.inputs())[1:]
+]
+print(traced_model)
+print(shape_list)
+
+mod, params = relay.frontend.pytorch.from_pytorch(
+    traced_model, shape_list, keep_quantized_weight=True
+)
 
 device = tvm.cpu()
 target = "llvm"
 dtype = "float32"
-ishape = (1, 3, 224, 224)
-mod, params = relay.testing.mobilenet.get_workload(batch_size=1, dtype="float32")
 print(mod["main"].astext(show_meta_data=False), "\n")
 mod = tachikoma.partition_for_tachikoma(mod, params)
 print(mod["main"].astext(show_meta_data=False), "\n")
