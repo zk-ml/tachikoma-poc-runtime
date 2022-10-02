597c4cb08425e7612389ae6733d317e16d92142c
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-10-02 15:12:44 -0400

    Update zen_quantized_lenet.py

--- experiments/zen_quantized_lenet.py
+++ experiments/zen_quantized_lenet.py
@@ -120,4 +120,4 @@ input_dict = {input_name : np.random.uniform(ishape).astype("float32")}
 with tvm.transform.PassContext(opt_level=1):
     func = relay.create_executor("vm", mod=mod, device=device, target=target).evaluate()
 
-func(**input, **params)
+func(**input_dict, **params)
