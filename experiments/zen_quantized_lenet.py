d49f462d9a961db88be1f1610b107ad613944a20
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-10-02 15:11:45 -0400

    Update zen_quantized_lenet.py

--- experiments/zen_quantized_lenet.py
+++ experiments/zen_quantized_lenet.py
@@ -115,18 +115,9 @@ print(mod["main"].astext(show_meta_data=False), "\n")
 print(mod.get_global_vars())
 print(type(mod))
 
+input_dict = {input_name : np.random.uniform(ishape).astype("float32")}
+
 with tvm.transform.PassContext(opt_level=1):
-    lib = relay.build(mod, target=target, params=params)
+    func = relay.create_executor("vm", mod=mod, device=device, target=target).evaluate()
 
-rmod = lib["default"](device)
-rt_mod = tvm.contrib.graph_executor.GraphModule(rmod)
-
-for i in range(2):    
-    print(len(lib.get_params()), " params")
-    for name, data in lib.get_params().items():
-        print(name, data.shape)
-        data = tvm.nd.array(data.numpy() + i)
-        rt_mod.set_input(name, data)
-    rt_mod.run()
-
-    out = rt_mod.get_output(0)
+func(**input, **params)
