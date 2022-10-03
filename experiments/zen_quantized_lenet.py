ab50d809898d36e126724aabd2ae5f904c29c900
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-10-02 21:24:01 -0400

    Update zen_quantized_lenet.py

--- experiments/zen_quantized_lenet.py
+++ experiments/zen_quantized_lenet.py
@@ -144,7 +144,6 @@ if DEBUG:
     with tvm.transform.PassContext(opt_level=1):
         lib = relay.build(mod, target=target, params=params)
     lib.export_library("./debug.so")
-    """
     m = GraphModuleDebug(
         lib["debug_create"]("default", device),
         [device],
@@ -157,7 +156,6 @@ if DEBUG:
     # execute
     m.run()
     tvm_out = m.get_output(0).numpy()
-    """
 else:
 
     with tvm.transform.PassContext(opt_level=1):
